3f5537b418494bc6689dce8f5ffef8c33ea190ef
Henry <111475591+Henryar7@users.noreply.github.com>
2022-08-27 12:18:59 -0500

    commit sprint 2

--- src/Perfil.java
+++ src/Perfil.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Perfil {
+public class Perfil {
+    private String id;
+    private String imagen;
+    private String telefono;
+
+    public Perfil(String id, String imagen, String telefono) {
+        this.id = id;
+        this.imagen = imagen;
+        this.telefono = telefono;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
